312a9abc69f2c4e966520fafa0eea5b35373f198
Shachar Sharon <synarete@gmail.com>
2023-08-24 12:09:50 +0300

    build: removed unused 'tools' subdir
    Do not confuse naive developer: remove unused 'tools' directory.
    
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- astyleall.sh
+++ astyleall.sh
@@ -6,8 +6,7 @@ set -o pipefail
 self=$(basename "${BASH_SOURCE[0]}")
 root=$(readlink -f "$(dirname "${self}")")
 srcs=$(find "${root}/include" "${root}/lib" \
-  "${root}/cmd" "${root}/mntd" \
-  "${root}/tests" "${root}/tools" \
+  "${root}/cmd" "${root}/mntd" "${root}/tests" \
   -type f -not -name "fuse_kernel.h" -not -name "configs.h" -name "*.[ch]")
 
 command -v astyle > /dev/null
--- cstylelint.sh
+++ cstylelint.sh
@@ -8,7 +8,6 @@ root=$(readlink -f "$(dirname "${self}")")
 checkcstyle_py="${root}"/scripts/checkcstyle.py
 
 srcs=$(find "${root}/include" "${root}/lib" \
-  "${root}/cmd" "${root}/mntd" \
-  "${root}/tools" "${root}/tests" \
+  "${root}/cmd" "${root}/mntd" "${root}/tests" \
   -type f -not -name "fuse_kernel.h" -not -name "configs.h" -name "*.[ch]")
 ${checkcstyle_py} ${srcs}

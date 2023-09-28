3024661b5d3b87d0a18e10477093473e7a4d7ff2
Shachar Sharon <synarete@gmail.com>
2023-09-28 17:29:34 +0300

    scripts/clangtidy: fixups using 'shellcheck'
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- scripts/clangtidy.sh
+++ scripts/clangtidy.sh
@@ -2,27 +2,29 @@
 
 _clang_tidy_check_list() {
   clang-tidy --list-checks -checks='*' | \
-    egrep -v 'Enabled checks' | \
-    egrep -v '(abseil|android|boost|cplusplus|osx|optin)' | \
-    egrep -v '(fuchsia|objc|zircon|security)' | \
-    egrep -v '(magic-numbers|hicpp-signed-bitwise|llvm-include-order)' | \
-    egrep -v '(cppcoreguidelines-init-variables)' | \
-    egrep -v '(lvmlibc-restrict-system-libc-headers)' | \
-    egrep -v '(Uninitialized|DeprecatedOrUnsafeBufferHandling)' | \
-    egrep -v 'readability-identifier-length' | \
-    egrep -v 'bugprone-easily-swappable-parameters' | \
+    grep -Ev 'Enabled checks' | \
+    grep -Ev '(abseil|android|boost|cplusplus|osx|optin)' | \
+    grep -Ev '(fuchsia|objc|zircon|security|altera)' | \
+    grep -Ev '(magic-numbers|hicpp-signed-bitwise|llvm-include-order)' | \
+    grep -Ev '(cppcoreguidelines-init-variables)' | \
+    grep -Ev '(lvmlibc-restrict-system-libc-headers)' | \
+    grep -Ev '(Uninitialized|DeprecatedOrUnsafeBufferHandling)' | \
+    grep -Ev 'readability-identifier-length' | \
+    grep -Ev 'bugprone-easily-swappable-parameters' | \
+    grep -Ev 'performance-no-int-to-ptr' | \
+    grep -Ev 'readability-suspicious-call-argument' | \
     awk '{print $1}' | \
     tr "\n" " "
 }
 
 _clang_tidy_check() {
-  basedir=$(realpath $(dirname ${BASH_SOURCE[0]})/../)
-  srcs=$(find ${basedir} -type f -name '*.c')
+  basedir=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../")
+  srcs=$(find "${basedir}" -type f -name '*.c')
   chks=$(_clang_tidy_check_list | tr " " ",")
-  idefs="-I${basedir}/include -I${basedir}/lib"
+  idefs="-I${basedir}/include -I${basedir}/lib -I${basedir}/build/include"
   xdefs="-DSILOFS_HAVE_PRIVATE=1 -DSILOFS_UNITEST=1"
 
-  clang-tidy ${srcs} -checks='-*',${chks} -- ${idefs} ${xdefs}
+  clang-tidy ${srcs} -checks='-*',${chks} -- "${idefs}" "${xdefs}"
 }
 
 _clang_tidy_version() {

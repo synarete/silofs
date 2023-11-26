990c2abd09c69b31bea8acc5ec1f13ff39461b03
Shachar Sharon <synarete@gmail.com>
2023-11-26 16:45:05 +0200

    fftests/test_fsync: test syncfs-by-ioctl
    Moved the syncfs-by-ioctl test into 'test_fsync.c' and don't use it as
    part of normal testing cycle. Specifically, don't expect such IOCTL to
    exist for other file-systems.
    
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- tests/fftests/test_fsync.c
+++ tests/fftests/test_fsync.c
@@ -20,41 +20,41 @@
 /*
  * Expects fsync(3p) to return 0 after regular file write/read operation.
  */
-static void test_fsync_reg(struct ft_env *fte, loff_t base_off,
-                           size_t bsz, loff_t step, size_t cnt)
+static void test_fsync_reg_(struct ft_env *fte, loff_t base_off,
+                            size_t bsz, loff_t step, size_t cnt)
 {
-	int fd = -1;
-	loff_t off = -1;
-	void *buf1 = NULL;
-	void *buf2 = ft_new_buf_rands(fte, bsz);
 	const char *path = ft_new_path_unique(fte);
+	void *buf1 = ft_new_buf_rands(fte, bsz);
+	void *buf2 = ft_new_buf_rands(fte, bsz);
+	loff_t off = -1;
+	int fd = -1;
 
 	ft_open(path, O_CREAT | O_RDWR, 0600, &fd);
 	for (size_t i = 0; i < cnt; ++i) {
 		off = base_off + ((loff_t)i  * step);
-		buf1 = ft_new_buf_rands(fte, bsz);
 		ft_pwriten(fd, buf1, bsz, off);
 		ft_fsync(fd);
 		ft_preadn(fd, buf2, bsz, off);
 		ft_expect_eqm(buf1, buf2, bsz);
 		buf2 = buf1;
+		buf1 = ft_new_buf_rands(fte, bsz);
 	}
 	ft_close(fd);
 	ft_unlink(path);
 }
 
-
 static void test_fsync_reg_aligned(struct ft_env *fte)
 {
-	test_fsync_reg(fte, 0, FT_1K, FT_1M, 64);
-	test_fsync_reg(fte, FT_1M, FT_BK_SIZE, FT_1G,
-	               64);
+	test_fsync_reg_(fte, 0, FT_1K, FT_1M, 100);
+	ft_relax_mem(fte);
+	test_fsync_reg_(fte, FT_1M, FT_64K, FT_1G, 100);
 }
 
 static void test_fsync_reg_unaligned(struct ft_env *fte)
 {
-	test_fsync_reg(fte, 1, FT_1K - 1, FT_1M + 1, 64);
-	test_fsync_reg(fte, FT_1M - 1, 3 * FT_1K + 1, FT_1G - 1, 64);
+	test_fsync_reg_(fte, 1, FT_1K - 1, FT_1M + 1, 100);
+	ft_relax_mem(fte);
+	test_fsync_reg_(fte, FT_1M - 1, 3 * FT_1K + 1, FT_1G - 1, 100);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -63,10 +63,10 @@ static void test_fsync_reg_unaligned(struct ft_env *fte)
  */
 static void test_fsync_dir_(struct ft_env *fte, size_t cnt)
 {
-	int fd = -1;
-	int dfd = -1;
 	const char *path1 = ft_new_path_unique(fte);
 	const char *path2 = NULL;
+	int dfd = -1;
+	int fd = -1;
 
 	ft_mkdir(path1, 0700);
 	ft_open(path1, O_DIRECTORY | O_RDONLY, 0, &dfd);
@@ -88,8 +88,12 @@ static void test_fsync_dir_(struct ft_env *fte, size_t cnt)
 
 static void test_fsync_dir(struct ft_env *fte)
 {
-	test_fsync_dir_(fte, 128);
-	test_fsync_dir_(fte, 4096);
+	const size_t cnt[] = { 10, 100, 10000 };
+
+	for (size_t i = 0; i < FT_ARRAY_SIZE(cnt); ++i) {
+		test_fsync_dir_(fte, cnt[i]);
+		ft_relax_mem(fte);
+	}
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -97,7 +101,7 @@ static void test_fsync_dir(struct ft_env *fte)
  * Expects successful fsync(3p) and stat(3p) on directory-fd in combination
  * with openat(2), renameat(2) and I/O calls.
  */
-static void test_fsync_dir_io_(struct ft_env *fte, size_t cnt, loff_t off_base)
+static void test_fsync_dir_io_(struct ft_env *fte, loff_t off_base, size_t cnt)
 {
 	struct stat st = { .st_size = -1 };
 	const char *path = ft_new_path_unique(fte);
@@ -144,23 +148,45 @@ static void test_fsync_dir_io_(struct ft_env *fte, size_t cnt, loff_t off_base)
 
 static void test_fsync_dir_io(struct ft_env *fte)
 {
-	test_fsync_dir_io_(fte, 128, FT_1M);
-	test_fsync_dir_io_(fte, 4096, FT_1T);
+	const loff_t off[] = { 0, FT_64K, FT_1M, FT_1T };
+	size_t cnt = 10;
+
+	for (size_t i = 0; i < FT_ARRAY_SIZE(off); ++i) {
+		test_fsync_dir_io_(fte, off[i], cnt);
+		cnt *= 10;
+		ft_relax_mem(fte);
+	}
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 /*
  * Expects syncfs(2) to return 0
  */
-static void test_fsync_syncfs(struct ft_env *fte)
+static void test_syncfs_simple(struct ft_env *fte)
 {
-	int fd = -1;
 	const char *path = ft_new_path_unique(fte);
+	int fd = -1;
 
 	ft_open(path, O_CREAT | O_RDWR, 0600, &fd);
 	ft_syncfs(fd);
 	ft_close(fd);
 	ft_unlink(path);
+}
+
+/*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
+/*
+ * Expects successful syncfs via ioctl.
+ */
+static void test_syncfs_by_ioctl(struct ft_env *fte)
+{
+	const char *path = ft_new_path_unique(fte);
+	int dfd = -1;
+
+	ft_mkdir(path, 0700);
+	ft_open(path, O_DIRECTORY | O_RDONLY, 0, &dfd);
+	ft_ioctl_syncfs(dfd);
+	ft_close(dfd);
+	ft_rmdir(path);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -170,7 +196,8 @@ static const struct ft_tdef ft_local_tests[] = {
 	FT_DEFTEST(test_fsync_reg_unaligned),
 	FT_DEFTEST(test_fsync_dir),
 	FT_DEFTEST(test_fsync_dir_io),
-	FT_DEFTEST(test_fsync_syncfs),
+	FT_DEFTEST(test_syncfs_simple),
+	FT_DEFTEST(test_syncfs_by_ioctl),
 };
 
 const struct ft_tests ft_test_fsync = FT_DEFTESTS(ft_local_tests);

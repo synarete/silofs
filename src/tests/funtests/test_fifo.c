0eaf43a54401b974a3518d26fc8d404baa055489
Shachar Sharon <synarete@gmail.com>
2024-03-11 12:21:03 +0200

    funtests/test_fifo: align with other tests conventions
    Minor cleanups to fifo tests.
    
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- src/tests/funtests/test_fifo.c
+++ src/tests/funtests/test_fifo.c
@@ -22,7 +22,7 @@
  */
 static void test_mkfifo(struct ft_env *fte)
 {
-	struct stat st;
+	struct stat st = { .st_mode = 0 };
 	const char *path = ft_new_path_unique(fte);
 
 	ft_mkfifo(path, S_IFIFO | 0600);
@@ -35,10 +35,10 @@ static void test_mkfifo(struct ft_env *fte)
 
 static void test_mkfifoat_(struct ft_env *fte, size_t count)
 {
-	int dfd;
-	struct stat st;
-	const char *name;
+	struct stat st = { .st_mode = 0 };
 	const char *path = ft_new_path_unique(fte);
+	const char *name = NULL;
+	int dfd = -1;
 
 	ft_mkdir(path, 0700);
 	ft_open(path, O_DIRECTORY | O_RDONLY, 0, &dfd);
@@ -60,8 +60,8 @@ static void test_mkfifoat_(struct ft_env *fte, size_t count)
 
 static void test_mkfifoat(struct ft_env *fte)
 {
-	test_mkfifoat_(fte, 16);
-	test_mkfifoat_(fte, 1024);
+	test_mkfifoat_(fte, 10);
+	test_mkfifoat_(fte, 1000);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -70,14 +70,14 @@ static void test_mkfifoat(struct ft_env *fte)
  */
 static void test_fifo_read_write_(struct ft_env *fte, size_t bsz)
 {
-	int wfd = -1;
-	int rfd = -1;
-	size_t nwr = 0;
-	size_t nrd = 0;
-	struct stat st;
+	struct stat st = { .st_mode = 0 };
 	void *buf1 = ft_new_buf_rands(fte, bsz);
 	void *buf2 = ft_new_buf_rands(fte, bsz);
 	const char *path = ft_new_path_unique(fte);
+	size_t nwr = 0;
+	size_t nrd = 0;
+	int wfd = -1;
+	int rfd = -1;
 
 	ft_mkfifo(path, S_IFIFO | 0600);
 	ft_stat(path, &st);
@@ -96,8 +96,8 @@ static void test_fifo_read_write_(struct ft_env *fte, size_t bsz)
 
 static void test_fifo_read_write(struct ft_env *fte)
 {
-	test_fifo_read_write_(fte, 1);
-	test_fifo_read_write_(fte, 4096);
+	test_fifo_read_write_(fte, 10);
+	test_fifo_read_write_(fte, 1000);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -106,11 +106,11 @@ static void test_fifo_read_write(struct ft_env *fte)
  */
 static void test_fifo_write_mctime_(struct ft_env *fte, size_t bsz)
 {
-	int fd = -1;
 	struct stat st[2];
 	void *buf1 = ft_new_buf_rands(fte, bsz);
 	void *buf2 = ft_new_buf_rands(fte, bsz);
 	const char *path = ft_new_path_unique(fte);
+	int fd = -1;
 	bool fuse_has_fifo_bug = true;
 
 	ft_mkfifo(path, S_IFIFO | 0600);
@@ -135,8 +135,8 @@ static void test_fifo_write_mctime_(struct ft_env *fte, size_t bsz)
 
 static void test_fifo_write_mctime(struct ft_env *fte)
 {
-	test_fifo_write_mctime_(fte, 512);
-	test_fifo_write_mctime_(fte, 4096);
+	test_fifo_write_mctime_(fte, 100);
+	test_fifo_write_mctime_(fte, 1000);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
@@ -145,13 +145,13 @@ static void test_fifo_write_mctime(struct ft_env *fte)
  */
 static void test_fifo_unlinked_(struct ft_env *fte, size_t bsz)
 {
-	int fd = -1;
-	int dfd = -1;
 	struct stat st[2];
 	void *buf1 = ft_new_buf_rands(fte, bsz);
 	void *buf2 = ft_new_buf_rands(fte, bsz);
 	const char *dpath = ft_new_path_unique(fte);
 	const char *path = ft_new_path_under(fte, dpath);
+	int dfd = -1;
+	int fd = -1;
 
 	ft_mkdir(dpath, 0700);
 	ft_open(dpath, O_DIRECTORY | O_RDONLY, 0, &dfd);
@@ -180,25 +180,24 @@ static void test_fifo_unlinked_(struct ft_env *fte, size_t bsz)
 
 static void test_fifo_unlinked(struct ft_env *fte)
 {
-	test_fifo_unlinked_(fte, 512);
-	test_fifo_unlinked_(fte, 4096);
+	test_fifo_unlinked_(fte, 100);
+	test_fifo_unlinked_(fte, 1000);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 /*
  * Expects a fifo to be alive as long as it is linked
  */
-static void test_fifo_nlinks_(struct ft_env *fte, nlink_t nlink)
+static void test_fifo_nlinks_(struct ft_env *fte, nlink_t nlink, size_t bsz)
 {
-	int fd = -1;
-	int dfd = -1;
-	struct stat st;
-	const size_t bsz = 1024;
+	struct stat st = { .st_mode = 0 };
 	void *buf1 = ft_new_buf_rands(fte, bsz);
 	void *buf2 = ft_new_buf_rands(fte, bsz);
 	const char *dpath = ft_new_path_unique(fte);
 	const char *fname = ft_make_ulong_name(fte, nlink);
 	const char *lname = NULL;
+	int dfd = -1;
+	int fd = -1;
 
 	ft_mkdir(dpath, 0700);
 	ft_open(dpath, O_DIRECTORY | O_RDONLY, 0, &dfd);
@@ -234,8 +233,8 @@ static void test_fifo_nlinks_(struct ft_env *fte, nlink_t nlink)
 
 static void test_fifo_nlinks(struct ft_env *fte)
 {
-	test_fifo_nlinks_(fte, 16);
-	test_fifo_nlinks_(fte, 1024);
+	test_fifo_nlinks_(fte, 10, 1000);
+	test_fifo_nlinks_(fte, 1000, 100);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/

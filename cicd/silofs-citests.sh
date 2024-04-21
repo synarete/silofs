1caf8640fbf08122fbb46ea4bb05dd07fb975fca
Shachar Sharon <synarete@gmail.com>
2024-04-21 19:59:20 +0300

    cicd: show delimiter between tests
    Be more friendly to en-user upon failures: show extra report details.
    
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- cicd/silofs-citests.sh
+++ cicd/silofs-citests.sh
@@ -5,7 +5,9 @@ set -o pipefail
 export LC_ALL=C
 unset CDPATH
 
+delim=". . . . . . . . . . . . . . . ."
 self=$(basename "${BASH_SOURCE[0]}")
+
 msg() { echo "$self: $*" >&2; }
 die() { msg "$*"; exit 1; }
 try() { ( "$@" ) || die "failed: $*"; }
@@ -17,14 +19,16 @@ archive_tgz="${dist_name}.tar.gz"
 workdir="${selfdir}/${dist_name}"
 
 # Require sane input
+msg "${delim}"
 cd "${selfdir}"
 run rm -rf "${workdir}"
 msg "check using ${archive_tgz}"
 run stat "${archive_tgz}"
 
 # Build from (clean) source
+msg "${delim}"
 cd "${selfdir}"
-run tar xvfz "${archive_tgz}"
+run tar xfz "${archive_tgz}"
 cd "${workdir}"
 msg "check build at: $(pwd)"
 run ./cstylefmt.sh
@@ -34,33 +38,35 @@ run make distcheck
 run make clean
 
 # Run developer's checks
+msg "${delim}"
 cd "${selfdir}"
 run rm -rf "${workdir}"
-run tar xvfz "${archive_tgz}"
+run tar xfz "${archive_tgz}"
 cd "${workdir}"
+msg "run unit-tests at: $(pwd)"
+run make -f devel.mk reset
+run make -f devel.mk O=2
+run make -f devel.mk O=2 check
 msg "run gcc-analyzer at: $(pwd)"
 run make -f devel.mk reset
 run make -f devel.mk O=0 CC=gcc ANALYZER=1
 msg "run clang-scan at: $(pwd)"
 run make -f devel.mk reset
 run make -f devel.mk V=1 O=2 clangscan
-msg "run unit-tests at: $(pwd)"
-run make -f devel.mk reset
-run make -f devel.mk O=2
-run make -f devel.mk O=2 check
 
 # Build dist-package
+msg "${delim}"
 cd "${selfdir}"
 run rm -rf "${workdir}"
-run tar xvfz "${archive_tgz}"
+run tar xfz "${archive_tgz}"
 cd "${workdir}"
 msg "build dist-package at: $(pwd)"
 run ./dist/packagize.sh
 
 # Post-op cleanup
+msg "${delim}"
 cd "${selfdir}"
 run rm -rf "${workdir}"
 msg "${dist_name} passed all checks"
-
 
 

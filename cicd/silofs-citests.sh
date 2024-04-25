2e00c7286bd69488c30155dfb19a0ef8f05cbd48
Shachar Sharon <synarete@gmail.com>
2024-04-25 12:52:56 +0300

    cicd: run valgrind over unitests
    Check no memory leaks using valgrind over unit-tests program.
    
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- cicd/silofs-citests.sh
+++ cicd/silofs-citests.sh
@@ -5,7 +5,7 @@ set -o pipefail
 export LC_ALL=C
 unset CDPATH
 
-delim=". . . . . . . . . . . . . . . ."
+delim="* * * * * * * * * * * * * * * *"
 self=$(basename "${BASH_SOURCE[0]}")
 
 msg() { echo "$self: $*" >&2; }
@@ -17,6 +17,7 @@ selfdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 dist_name="$1"
 archive_tgz="${dist_name}.tar.gz"
 workdir="${selfdir}/${dist_name}"
+unitestsdir="${workdir}/build/src/tests/unitests/"
 
 # Require sane input
 msg "${delim}"
@@ -43,16 +44,21 @@ cd "${selfdir}"
 run rm -rf "${workdir}"
 run tar xfz "${archive_tgz}"
 cd "${workdir}"
-msg "run unit-tests at: $(pwd)"
+msg "run unit-tests"
 run make -f devel.mk reset
 run make -f devel.mk O=2
 run make -f devel.mk O=2 check
-msg "run gcc-analyzer at: $(pwd)"
+msg "run gcc-analyzer"
 run make -f devel.mk reset
 run make -f devel.mk O=0 CC=gcc ANALYZER=1
-msg "run clang-scan at: $(pwd)"
+msg "run clang-scan"
 run make -f devel.mk reset
 run make -f devel.mk V=1 O=2 clangscan
+msg "run valgrind check"
+run make -f devel.mk reset
+run make -f devel.mk
+run valgrind --tool=memcheck --error-exitcode=1 \
+  "${unitestsdir}/silofs-unitests" "${unitestsdir}/ut" -M -l1
 
 # Build dist-package
 msg "${delim}"

ea53fcdcbf690b4138134e7ce3a8f2a79de566e0
Shachar Sharon <synarete@gmail.com>
2024-11-25 17:20:01 +0200

    scripts/clangformat: do not change dir to project's root
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- scripts/clangformat.sh
+++ scripts/clangformat.sh
@@ -3,6 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 root=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
-cd "${root}"
+
 command -v clang-format > /dev/null
-clang-format -i -style=file "$@"
+clang-format -i --style=file:"${root}/.clang-format" "$@"

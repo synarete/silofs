cbb46d2e95eece9d881cf72c8adb05d44476ab27
Shachar Sharon <synarete@gmail.com>
2025-02-13 18:22:16 +0200

    scripts/clangtidy: use run helper functions
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- scripts/clangtidy.sh
+++ scripts/clangtidy.sh
@@ -5,23 +5,30 @@ set -o pipefail
 export LC_ALL=C
 unset CDPATH
 
+self=$(basename "${BASH_SOURCE[0]}")
+msg() { echo "$self: $*" >&2; }
+die() { msg "$*"; exit 1; }
+exe() { ( "$@" ) || die "failed: $*"; }
+run() { echo "$self:" "$@" >&2; exe "$@"; }
+cdx() { echo "$self: cd $*" >&2; cd "$@" || die "failed: cd $*"; }
+
 # run from project's root dir
 basedir=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../")
-cd "${basedir}"
+cdx "${basedir}"
 
 # require clang-tidy utility
-command -v clang-tidy &> /dev/null
+run command -v clang-tidy &> /dev/null
 
 # require bear utility to generate compilation database for clang tooling
-command -v bear &> /dev/null
+run command -v bear &> /dev/null
 
 # require compilation database
 if [ ! -f "${basedir}/compile_commands.json" ]; then
-  make -f devel.mk reset
-  bear -- make -f devel.mk CC=clang
+  run make -f devel.mk reset
+  run bear -- make -f devel.mk CC=clang
 fi
 
 # run clang-tidy
 conf="${basedir}/.clang-tidy.yaml"
 srcs=$(find "${basedir}/" -type f -name '*.c')
-clang-tidy --config-file="${conf}" ${srcs} 2> /dev/null
+run clang-tidy --config-file="${conf}" ${srcs}

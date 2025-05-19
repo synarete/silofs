2ec6075f9b9c26c8f0173b4ba3e7e1e02c4f8f76
Shachar Sharon <synarete@gmail.com>
2025-05-19 21:58:52 +0300

    ftests: allow extra block
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- test/ftests/ftests_run.c
+++ test/ftests/ftests_run.c
@@ -128,7 +128,7 @@ static void verify_consistent_statvfs(const struct statvfs *stv_beg,
 	ft_expect_ge(stv_beg->f_bavail, stv_end->f_bavail);
 
 	bfree_dif = stv_beg->f_bfree - stv_end->f_bfree;
-	ft_expect_lt(bfree_dif, 4096);
+	ft_expect_lt(bfree_dif, 8192);
 }
 
 static bool ft_without_statvfs(const struct ft_env *fte)

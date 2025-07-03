7bd2ae544985b08e5008c145fbbc516480de1466
Shachar Sharon <synarete@gmail.com>
2025-07-03 18:24:33 +0300

    utests: be more silent in level=1
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- test/utests/utests_main.c
+++ test/utests/utests_main.c
@@ -94,9 +94,13 @@ static void ut_setup_globals(int argc, char *argv[])
 
 static void ut_setup_logging(void)
 {
-	ut_globals.log_params.level = SILOFS_LOG_INFO;
-	ut_globals.log_params.flags = SILOFS_LOGF_VERBOSE | SILOFS_LOGF_STDOUT;
+	ut_globals.log_params.level = SILOFS_LOG_WARN;
+	ut_globals.log_params.flags = SILOFS_LOGF_STDOUT;
 
+	if (ut_globals.run_level >= 2) {
+		ut_globals.log_params.level = SILOFS_LOG_INFO;
+		ut_globals.log_params.flags |= SILOFS_LOGF_VERBOSE;
+	}
 	if (ut_globals.timestamp) {
 		ut_globals.log_params.flags |= SILOFS_LOGF_TIMESTAMP;
 	}

37155a5c3bdd1953ace85d4c577a39fdefdd7de1
Shachar Sharon <synarete@gmail.com>
2025-11-10 16:57:03 +0200

    ftests: test stat on sparse file
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- test/ftests/ft_rw_sparse.c
+++ test/ftests/ft_rw_sparse.c
@@ -113,7 +113,7 @@ static void test_rw_sparse_overwrite_(struct ft_env *fte, off_t base_off)
 	uint8_t *buf1 = ft_new_buf_rands(fte, len1);
 	uint8_t *buf2 = ft_new_buf_rands(fte, len2);
 	uint8_t *buf3 = ft_new_buf_rands(fte, len1 + len2);
-	char *path = ft_new_path_unique(fte);
+	const char *path = ft_new_path_unique(fte);
 	const off_t offs[] = {
 		737717, 280411, 10007, 31033,  42043,   53113, 161881,  375533,
 		86767,  97171,  75353, 611999, 1108007, 64601, 1272211, 20323,
@@ -154,11 +154,71 @@ static void test_rw_sparse_overwrite(struct ft_env *fte)
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
+/*
+ * Test st_blocks vs st_size on sparse files.
+ */
+static void test_rw_sparse_stat_(struct ft_env *fte, off_t from_off,
+                                 size_t data_size, size_t skip_size)
+{
+	struct stat st = { .st_size = -1 };
+	const size_t nsteps = 8;
+	const char *path = ft_new_path_unique(fte);
+	const char *name = ft_new_name_unique(fte);
+	uint8_t *buf1 = ft_new_buf_rands(fte, data_size);
+	uint8_t *buf2 = ft_new_buf_zeros(fte, data_size);
+	ssize_t size = -1;
+	off_t off = -1;
+	int dfd = -1;
+	int fd = -1;
+
+	ft_mkdir(path, 0700);
+	ft_open(path, O_DIRECTORY | O_RDONLY, 0, &dfd);
+	ft_openat(dfd, name, O_CREAT | O_RDWR, 0600, &fd);
+	for (size_t i = 0; i < nsteps; ++i) {
+		buf1[0] = (uint8_t)i;
+		buf2[0] = 0xFF - (uint8_t)i;
+		off = from_off + (ssize_t)(i * skip_size);
+		ft_pwriten(fd, buf1, data_size, off);
+		size = off + (ssize_t)data_size;
+		ft_fstat(fd, &st);
+		ft_expect_eq(st.st_size, size);
+		ft_expect_lt(st.st_blocks * 512, size);
+		ft_ftruncate(fd, (loff_t)size + 1);
+		ft_preadn(fd, buf2, data_size, off);
+		ft_expect_eqm(buf1, buf2, data_size);
+		ft_preadn(fd, &buf2[0], 1, (off_t)size);
+		ft_expect_eq(buf2[0], 0);
+	}
+	ft_ftruncate(fd, 0);
+	ft_ftruncate(fd, (off_t)size);
+	ft_fstat(fd, &st);
+	ft_expect_eq(st.st_size, size);
+	ft_expect_eq(st.st_blocks, 0);
+	ft_close(fd);
+	ft_unlinkat(dfd, name, 0);
+	ft_close(dfd);
+	ft_rmdir(path);
+}
+
+static void test_rw_sparse_stat(struct ft_env *fte)
+{
+	test_rw_sparse_stat_(fte, FT_1K, FT_1K, FT_1M);
+	test_rw_sparse_stat_(fte, FT_4K, FT_1K, FT_1M);
+	test_rw_sparse_stat_(fte, FT_4K, FT_4K, FT_1M);
+	test_rw_sparse_stat_(fte, FT_64K, FT_4K, FT_1M);
+	test_rw_sparse_stat_(fte, FT_1M, FT_4K, FT_1G);
+	test_rw_sparse_stat_(fte, FT_1G, FT_64K, FT_1G);
+	test_rw_sparse_stat_(fte, FT_1T, FT_1M, FT_1G);
+	test_rw_sparse_stat_(fte, FT_1G - 1, FT_64K + 11, FT_1M + 111);
+}
+
+/*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
 static const struct ft_tdef ft_local_tests[] = {
 	FT_DEFTEST(test_rw_sparse_simple),
 	FT_DEFTEST(test_rw_sparse_repeat),
 	FT_DEFTEST(test_rw_sparse_overwrite),
+	FT_DEFTEST(test_rw_sparse_stat),
 };
 
 const struct ft_tests ft_test_rw_sparse = FT_DEFTESTS(ft_local_tests);

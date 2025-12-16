b3a401b374ae7daba29765a441c7e96b613c5c98
Shachar Sharon <synarete@gmail.com>
2025-12-16 18:08:42 +0200

    version.sh: ignore annotated tags
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- version.sh
+++ version.sh
@@ -10,7 +10,7 @@ export GIT_DIR="${base}/.git"
 gittop=$(git rev-parse --show-toplevel > /dev/null 2>&1 && echo -n "git-repo")
 
 if [ -n "${gittop}" ]; then
-	gitrevision=$(git describe --abbrev=8 --always --dirty=+)
+	gitrevision=$(git describe --always --dirty=+ --abbrev=8 --exclude '*')
 else
 	gitrevision=""
 fi

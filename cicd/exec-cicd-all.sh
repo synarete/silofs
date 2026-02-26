e026b17a82341e19dc0f87795a67aad29af5481e
Shachar Sharon <synarete@gmail.com>
2026-02-26 12:21:10 +0200

    cicd: run sub-commands with 30sec timeout
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- cicd/exec-cicd-all.sh
+++ cicd/exec-cicd-all.sh
@@ -9,7 +9,11 @@ self="$(basename "${BASH_SOURCE[0]}")"
 selfdir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 rootdir="$(realpath "${selfdir}"/../)"
 source "${rootdir}/bash_functions"
+commandv make tar timeout
+
+###
 sep() { msg "$*" ; echo "# # # # # # # # # # # # # # # # " ; }
+exe() { timeout -k 10s 30m runx $* ; }
 
 ###
 if [ "$#" -ne 2 ]; then die "usage: '$self <archive-file> <citests-dir>'"; fi
@@ -20,8 +24,8 @@ currdir="$(pwd)"
 ###
 msg "checking input: $*"
 cdx "${currdir}"
-runx test -f "${archive_file}"
-runx mkdir -p "${citests_dir}"
+exe test -f "${archive_file}"
+exe mkdir -p "${citests_dir}"
 run ls "${citests_dir}"
 sep "input OK"
 
@@ -31,24 +35,24 @@ dist_name="$(basename -s .tar.gz "${archive_file}")"
 archive_tgz="${dist_name}.tar.gz"
 workdir="${citests_dir}/${dist_name}"
 utestsdir="${workdir}/build/test/utests/"
-runx mkdir -p "${workdir}"
-runx rm -rf "${workdir}"
+exe mkdir -p "${workdir}"
+exe rm -rf "${workdir}"
 sep "workdir OK: ${workdir}"
 
 ###
 msg "build from source: ${archive_file}"
 cdx "${currdir}"
-runx cp "${archive_file}" "${citests_dir}"
+runt cp "${archive_file}" "${citests_dir}"
 cdx "${citests_dir}"
-runx tar xfz "${archive_tgz}"
+exe tar xfz "${archive_tgz}"
 cdx "${workdir}"
 msg "check code style: $(pwd)"
-runx ./scripts/checkcodefmt.sh
+exe ./scripts/checkcodefmt.sh
 msg "check build at: $(pwd)"
-runx ./configure
-runx make
-runx make distcheck
-runx make clean
+exe ./configure
+exe make
+exe make distcheck
+exe make clean
 cdx "${currdir}"
 run rm -rf "${workdir}"
 sep "source build OK: ${archive_file}"
@@ -58,76 +62,76 @@ msg "run developer's checks"
 cdx "${currdir}"
 run cp "${archive_file}" "${citests_dir}"
 cdx "${citests_dir}"
-runx tar xfz "${archive_tgz}"
+exe tar xfz "${archive_tgz}"
 cdx "${workdir}"
 msg "build default mode"
-runx make -f devel.mk
-runx make -f devel.mk reset
+exe make -f devel.mk
+exe make -f devel.mk reset
 msg "build with analyzer"
-runx make -f devel.mk O=0 ANALYZER=1
-runx make -f devel.mk reset
+exe make -f devel.mk O=0 ANALYZER=1
+exe make -f devel.mk reset
 msg "run unit-tests"
-runx env SILOFS_PANIC_MODE_WAIT=1 make -f devel.mk check
-runx make -f devel.mk reset
+exe env SILOFS_PANIC_MODE_WAIT=1 make -f devel.mk check
+exe make -f devel.mk reset
 msg "run clang-scan"
-runx make -f devel.mk CC=clang V=1 O=2 scan
-runx make -f devel.mk reset
+exe make -f devel.mk CC=clang V=1 O=2 scan
+exe make -f devel.mk reset
 sep "developer's build OK"
 
 ###
 msg "run clang-tidy"
-runx make -f devel.mk CC=clang O=2 tidy
-runx make -f devel.mk reset
+exe make -f devel.mk CC=clang O=2 tidy
+exe make -f devel.mk reset
 sep "clang-tidy OK"
 
 ###
 msg "run sanitizer check"
 lsan_suppressions_file="${workdir}/test/utests/lsan_suppressions.txt"
-runx make -f devel.mk O=1 SANITIZER=1
-runx env ASAN_OPTIONS=detect_leaks=1 \
+exe make -f devel.mk O=1 SANITIZER=1
+exe env ASAN_OPTIONS=detect_leaks=1 \
 	LSAN_OPTIONS=suppressions="${lsan_suppressions_file}" \
 	"${utestsdir}/silofs-utests" "${utestsdir}/ut" \
 	--malloc --level=1 --silent
-runx make -f devel.mk reset
+exe make -f devel.mk reset
 sep "sanitizer OK"
 
 ###
 msg "run valgrind check"
-runx make -f devel.mk
-runx valgrind --tool=memcheck --error-exitcode=1 \
+exe make -f devel.mk
+exe valgrind --tool=memcheck --error-exitcode=1 \
 	"${utestsdir}/silofs-utests" "${utestsdir}/ut" \
 	--malloc --level=1 --silent
-runx make -f devel.mk reset
+exe make -f devel.mk reset
 sep "valgrind OK"
 
 ###
 cdx "${workdir}"
 msg "run heap checker to detect memory leaks"
-runx ./bootstrap
-runx mkdir -p "${workdir}/build/local/tmp"
+exe ./bootstrap
+exe mkdir -p "${workdir}/build/local/tmp"
 cdx "${workdir}/build"
-runx ../configure --prefix="${workdir}/build/local" \
+exe ../configure --prefix="${workdir}/build/local" \
 	--enable-compile-warnings=error --with-tcmalloc
-runx make install
+exe make install
 # TODO: fails on ubuntu; why?
-runx env HEAPCHECK=normal HEAP_CHECK_TEST_POINTER_ALIGNMENT=1 \
+exe env HEAPCHECK=normal HEAP_CHECK_TEST_POINTER_ALIGNMENT=1 \
 	"${workdir}/build/local/bin/silofs-utests" \
 	"${workdir}/build/local/tmp" \
 	--malloc --level=2 --silent
 cdx "${currdir}"
-runx rm -rf "${workdir}"
+exe rm -rf "${workdir}"
 sep "heapcheck OK"
 
 ###
 msg "build dist-package"
 cdx "${currdir}"
-runx cp "${archive_file}" "${citests_dir}"
+exe cp "${archive_file}" "${citests_dir}"
 cdx "${citests_dir}"
-runx tar xfz "${archive_tgz}"
+exe tar xfz "${archive_tgz}"
 cdx "${workdir}"
-runx ./dist/packagize.sh
+exe ./dist/packagize.sh
 cdx "${currdir}"
-runx rm -rf "${workdir}"
+exe rm -rf "${workdir}"
 sep "dist-package OK"
 
 ###

f93f51e0c14d420747de9ce0df2056c0fbf930d0
Shachar Sharon <synarete@gmail.com>
2026-05-13 17:53:55 +0300

    scripts/clangscan: compile without debug
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- scripts/clangscan.sh
+++ scripts/clangscan.sh
@@ -15,7 +15,8 @@ source "${rootdir}/bash_functions"
 _clang_scan_enabled_checkers_args() {
 	clang -cc1 -analyzer-checker-help \
 		| awk '/^  [A-Za-z0-9_.]+/{print $1}' \
-		| grep -Ev '^(osx|fuchsia|cplusplus|optin|strcpy|webkit|alpha|experimental|debug)' \
+		| grep -Ev '^(osx|fuchsia|cplusplus|optin|strcpy|webkit)' \
+		| grep -Ev '^(alpha|experimental|debug)' \
 		| grep -Ev 'DeprecatedOrUnsafeBufferHandling' \
 		| grep -Ev 'valist\.Uninitialized' \
 		| grep -Ev 'security\.VAList' \
@@ -48,7 +49,9 @@ _clang_scan_build() {
 	run scan-build \
 		--use-cc="${CCC_CC}" \
 		--use-c++="${CCC_CXX}" \
-		"${topdir}/configure" CFLAGS='-O2 -pthread'
+		"${topdir}/configure" \
+		--enable-debug=no \
+		CFLAGS='-O2 -pthread'
 
 	run scan-build \
 		--use-cc="${CCC_CC}" \

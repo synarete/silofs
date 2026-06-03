47613c4952057f5f83f0fb4c731cd7095038c23d
Shachar Sharon <synarete@gmail.com>
2026-06-03 16:49:36 +0300

    utests: symlink with nfiles
    Signed-off-by: Shachar Sharon <synarete@gmail.com>

--- test/utests/ut_symlink.c
+++ test/utests/ut_symlink.c
@@ -23,9 +23,9 @@ static const char *ut_make_symname(struct ut_env *ute, size_t idx)
 
 static char *ut_make_symval_with(struct ut_env *ute, char c, size_t len)
 {
-	const size_t vsz      = SILOFS_PATH_MAX;
-	const size_t name_max = NAME_MAX;
-	char *val             = nullptr;
+	constexpr size_t vsz      = SILOFS_PATH_MAX;
+	constexpr size_t name_max = NAME_MAX;
+	char *val;
 
 	ut_expect_le(len, vsz);
 	val = (char *)ut_zerobuf(ute, vsz + 1);
@@ -53,9 +53,7 @@ static void ut_symlink_simple(struct ut_env *ute)
 	const char *dname = UT_NAME;
 	const char *tname = "target";
 	const char *sname = "symlink";
-	ino_t dino        = 0;
-	ino_t tino        = 0;
-	ino_t sino        = 0;
+	ino_t dino = 0, tino = 0, sino = 0;
 
 	ut_mkdir_at_root(ute, dname, &dino);
 	ut_mkdir2(ute, dino, tname, &tino);
@@ -79,8 +77,7 @@ static void ut_symlink_length(struct ut_env *ute)
 	const char *sname    = nullptr;
 	const size_t nlinks  = SILOFS_PATH_MAX - 1;
 	const ino_t root_ino = UT_ROOT_INO;
-	ino_t dino           = 0;
-	ino_t sino           = 0;
+	ino_t dino = 0, sino = 0;
 
 	ut_mkdir2(ute, root_ino, dname, &dino);
 	for (size_t i = 1; i <= nlinks; ++i) {
@@ -100,8 +97,8 @@ static void ut_symlink_length(struct ut_env *ute)
 
 static void ut_symlink_nested(struct ut_env *ute)
 {
-	ino_t sino = 0;
 	ino_t dino[128];
+	ino_t sino = 0;
 	const char *sname[128];
 	const char *dname = UT_NAME;
 	struct stat st    = { .st_size = -1 };
@@ -127,12 +124,10 @@ static void ut_symlink_nested(struct ut_env *ute)
 
 static void ut_symlink_to_reg_(struct ut_env *ute, size_t cnt)
 {
-	ino_t dino        = 0;
-	ino_t ino         = 0;
-	ino_t sino        = 0;
 	const char *dname = UT_NAME;
 	const char *fname = nullptr;
 	const char *sname = nullptr;
+	ino_t dino = 0, ino = 0, sino = 0;
 
 	ut_mkdir_at_root(ute, dname, &dino);
 	for (size_t i = 0; i < cnt; ++i) {
@@ -164,7 +159,7 @@ static void ut_symlink_to_reg(struct ut_env *ute)
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
-static void ut_symlink_and_io_(struct ut_env *ute, size_t cnt)
+static void ut_symlink_with_io_(struct ut_env *ute, size_t cnt)
 {
 	off_t off         = -1;
 	ino_t dino        = 0;
@@ -211,15 +206,15 @@ static void ut_symlink_and_io_(struct ut_env *ute, size_t cnt)
 	ut_rmdir_at_root(ute, dname);
 }
 
-static void ut_symlink_and_io(struct ut_env *ute)
+static void ut_symlink_with_io(struct ut_env *ute)
 {
-	ut_symlink_and_io_(ute, 128);
-	ut_symlink_and_io_(ute, SILOFS_SYMLNK_MAX);
+	ut_symlink_with_io_(ute, 128);
+	ut_symlink_with_io_(ute, SILOFS_SYMLNK_MAX);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
-static void ut_symlink_and_io2_(struct ut_env *ute, size_t cnt)
+static void ut_symlink_with_io2_(struct ut_env *ute, size_t cnt)
 {
 	const char *fname    = nullptr;
 	const char *sname    = nullptr;
@@ -280,23 +275,23 @@ static void ut_symlink_and_io2_(struct ut_env *ute, size_t cnt)
 	ut_rmdir(ute, root_ino, dname);
 }
 
-static void ut_symlink_and_io2(struct ut_env *ute)
+static void ut_symlink_with_io2(struct ut_env *ute)
 {
-	ut_symlink_and_io2_(ute, 11);
-	ut_symlink_and_io2_(ute, 111);
-	ut_symlink_and_io2_(ute, 1111);
-	ut_symlink_and_io2_(ute, SILOFS_SYMLNK_MAX);
+	ut_symlink_with_io2_(ute, 11);
+	ut_symlink_with_io2_(ute, 111);
+	ut_symlink_with_io2_(ute, 1111);
+	ut_symlink_with_io2_(ute, SILOFS_SYMLNK_MAX);
 }
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
 static blkcnt_t symval_length_to_blocks(size_t len)
 {
-	size_t nparts         = 0;
-	blkcnt_t blkcnt       = 0;
-	const size_t val_size = SILOFS_SYMLNK_VAL_SIZE;
-	const size_t head_len = SILOFS_SYMLNK_HEAD_MAX;
-	const size_t factor   = val_size / 512;
+	constexpr size_t val_size = SILOFS_SYMLNK_VAL_SIZE;
+	constexpr size_t head_len = SILOFS_SYMLNK_HEAD_MAX;
+	constexpr size_t factor   = val_size / 512;
+	size_t nparts             = 0;
+	blkcnt_t blkcnt           = 0;
 
 	if (len > head_len) {
 		nparts = silofs_div_round_up(len - head_len, val_size);
@@ -311,8 +306,7 @@ static void ut_symlink_stat_(struct ut_env *ute, size_t valsize)
 	const char *name      = UT_NAME;
 	const char *symval    = ut_make_symval_with(ute, 's', valsize);
 	const blkcnt_t blocks = symval_length_to_blocks(valsize);
-	ino_t dino            = 0;
-	ino_t sino            = 0;
+	ino_t dino = 0, sino = 0;
 
 	ut_mkdir_at_root(ute, name, &dino);
 	ut_symlink(ute, dino, name, symval, &sino);
@@ -338,14 +332,61 @@ static void ut_symlink_stat(struct ut_env *ute)
 
 /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
+static void ut_symlink_nfiles_(struct ut_env *ute, size_t nfiles)
+{
+	const char *dname = UT_NAME;
+	const char *fname = nullptr;
+	const char *sname = nullptr;
+	ino_t dino = 0, ino = 0, sino = 0;
+
+	ut_mkdir_at_root(ute, dname, &dino);
+	for (size_t i = 0; i < nfiles; ++i) {
+		sname = ut_make_symname(ute, i);
+		fname = ut_make_name(ute, dname, i);
+		ut_create_only(ute, dino, fname, &ino);
+		ut_symlink(ute, dino, sname, fname, &sino);
+	}
+	ut_sync_drop_all(ute);
+
+	for (size_t i = 0; i < nfiles; ++i) {
+		sname = ut_make_symname(ute, i);
+		fname = ut_make_name(ute, dname, i);
+		ut_lookup_ino(ute, dino, sname, &sino);
+		ut_lookup_lnk(ute, dino, sname, sino);
+		ut_readlink_expect(ute, sino, fname);
+		ut_unlink(ute, dino, sname);
+	}
+	ut_sync_drop_all(ute);
+
+	for (size_t i = 0; i < nfiles; ++i) {
+		sname = ut_make_symname(ute, i);
+		fname = ut_make_name(ute, dname, i);
+		ut_lookup_noent(ute, dino, sname);
+		ut_unlink(ute, dino, fname);
+	}
+	ut_rmdir_at_root(ute, dname);
+}
+
+static void ut_symlink_nfiles(struct ut_env *ute)
+{
+	ut_symlink_nfiles_(ute, 10);
+	ut_symlink_nfiles_(ute, SILOFS_BTREE_NODE_NCHILDS);
+	ut_symlink_nfiles_(ute, 2 * SILOFS_BTREE_NODE_NCHILDS);
+	ut_symlink_nfiles_(ute, SILOFS_SPNODE_NREFS);
+	ut_symlink_nfiles_(ute, 2 * SILOFS_SPNODE_NREFS);
+}
+
+/*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
+
 static const struct ut_testdef ut_local_tests[] = {
-	UT_DEFTEST1(ut_symlink_simple), //
-	UT_DEFTEST(ut_symlink_length),  //
-	UT_DEFTEST(ut_symlink_nested),  //
-	UT_DEFTEST(ut_symlink_to_reg),  //
-	UT_DEFTEST(ut_symlink_and_io),  //
-	UT_DEFTEST(ut_symlink_and_io2), //
-	UT_DEFTEST(ut_symlink_stat),    //
+	UT_DEFTEST1(ut_symlink_simple),  //
+	UT_DEFTEST(ut_symlink_length),   //
+	UT_DEFTEST(ut_symlink_nested),   //
+	UT_DEFTEST(ut_symlink_to_reg),   //
+	UT_DEFTEST(ut_symlink_with_io),  //
+	UT_DEFTEST(ut_symlink_with_io2), //
+	UT_DEFTEST(ut_symlink_stat),     //
+	UT_DEFTEST(ut_symlink_nfiles),   //
 };
 
 const struct ut_testdefs ut_tdefs_symlink = UT_MKTESTS(ut_local_tests);
